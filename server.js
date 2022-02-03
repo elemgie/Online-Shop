617ef27dc02548187848237817776c636c359baa
Maciej Witkowski <maciej.witkowski@iwop.pl>
2022-02-03 23:35:29 +0100

    impemented basic routing

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.listen(port);
+console.log(`Listening on 127.0.0.1:${port}`);
