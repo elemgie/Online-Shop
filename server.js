19d8c4d28f9a939801ec0d86f053082e76aa8035
Maciej Witkowski <maciej.witkowski@iwop.pl>
2022-02-04 13:58:04 +0100

    implemented login and registration forms

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use( express.static( "public" ) );
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('pages/index');
+});
+
+app.get('/register', function(req, res) {
+    res.render('pages/register');
+  });
+  
+app.get('/login', function(req, res) {
+    res.render('pages/login');
 });
 
 app.listen(port);
