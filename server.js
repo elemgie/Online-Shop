a5a6f7aec6152199e081f75f01f2dc4b1a0b3fbb
Maciej Witkowski <maciej.witkowski@iwop.pl>
2022-02-04 22:16:23 +0100

    implemented basic views - list and detail views

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+let productList = [
+  {
+    id: 'U23093249028409238049',
+    name: "Produkt przykładowy",
+    price: 134.24,
+    description: "Lin ke lsk djflkgj d lkgjfdl kgjdf kjgj fdoigj dfkjgdknikdfn gifd gkjdf o dfkfdk j kfdgjikfd kjfd nkdfj kdfjj dfk fdj gjdf dfk dfnoidfgn kdfjngdf m lkdmfolgm dfljdifj lkdf m gkjfdngkfdhglk dfjgkndfo gho",
+    quantity: 2455
+  },
+  {
+    id: 'U23093249sdf028409238049',
+    name: "Marchewa",
+    price: 134.24,
+    description: "Lin ke lsk djflkgj d lkgjfdl kgjdf kjgj fdoigj dfkjgdknikdfn gifd gkjdf o dfkfdk j kfdgjikfd kjfd nkdfj kdfjj dfk fdj gjdf dfk dfnoidfgn kdfjngdf m lkdmfolgm dfljdifj lkdf m gkjfdngkfdhglk dfjgkndfo gho",
+    quantity: 300
+  },
+  {
+    id: 'U2309312dsfdf3409238049',
+    name: "Trampki",
+    price: 134.24,
+    description: "Lin ke lsk djflkgj d lkgjfdl kgjdf kjgj fdoigj dfkjgdknikdfn gifd gkjdf o dfkfdk j kfdgjikfd kjfd nkdfj kdfjj dfk fdj gjdf dfk dfnoidfgn kdfjngdf m lkdmfolgm dfljdifj lkdf m gkjfdngkfdhglk dfjgkndfo gho",
+    quantity: 300
+  }
+]
+
+const getProductById = id => productList.filter(obj => obj.id == id)[0];
 
 app.use( express.static( "public" ) );
 app.set('view engine', 'ejs');
@@ -15,6 +40,25 @@ app.get('/register', function(req, res) {
   
 app.get('/login', function(req, res) {
     res.render('pages/login');
+});
+
+app.get('/products', function(req, res) {
+  if(req.query.search && req.query.search != ""){
+    res.render('pages/products', {
+      products: productList.filter(product => product.name.toLowerCase().indexOf(req.query.search.toLowerCase()) != -1)
+    });
+  } else {
+    res.render('pages/products', {
+      products: productList
+    });
+  }
+  
+});
+
+app.get('/product/:id', function(req, res) {
+  res.render('pages/product',{
+    product: getProductById(req.params.id)
+  });
 });
 
 app.listen(port);
